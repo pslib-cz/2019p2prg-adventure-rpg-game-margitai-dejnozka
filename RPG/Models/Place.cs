eef508a5198b33fcd235d55cc4299c4979a276b1
Filip Margitai <u18.415@pslib.cloud>
2020-05-21 21:09:43 +0200

    ready for pull

---
+++ RPG/Models/Place.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RPG_Game.Models
+{
+    public class Place
+    {
+        public string Name { get; }
+        public string Description { get; }
+        public List<Place> TravelPsb = new List<Place>();
+
+        public Place(string name, string Description)
+        {
+            Name = name;
+            Description = description;
+        }
+        public void AddTravelPsb(Place newPsb)
+        {
+            TravelPsb.Add(newPsb);
+        }
+        public void RmvTravelPsb(Place delPsb)
+        {
+            for (int i = 0; i < TravelPsb.Count; i++)
+            {
+                if (TravelPsb[i].Name == delPsb.Name)
+                {
+                    TravelPsb.RemoveAt(i);
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ RPG/Pages/Index.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using RPG_Game.Models;
+
+namespace RPG.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        public Player player = new Player("Smazak", 100, new Place("cesta", "Nacházíš se na cestě lemované borovicemi"));
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
