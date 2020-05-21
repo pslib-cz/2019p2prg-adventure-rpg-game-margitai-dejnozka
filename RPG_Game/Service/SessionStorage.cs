91df642a164bb6c68f10bd65853add42e8f6e00e
Filip Margitai <u18.415@pslib.cloud>
2020-05-21 19:15:56 +0200

    new session

---
+++ RPG_Game/Service/SessionStorage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Session.Service
+{
+    public class SessionStorage
+    {
+        readonly ISession _session;
+        public SessionStorage(IHttpContextAccessor hca)
+        {
+            _session = hca.HttpContext.Session;
+        }
+    }
+}
--- RPG_Game/Startup.cs
+++ RPG_Game/Startup.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -23,6 +24,12 @@ namespace RPG_Game
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDistributedMemoryCache();
+            services.AddSession();
+
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddScoped<Service.SessionStorage>();
+
             services.AddRazorPages();
         }
 
@@ -46,6 +53,7 @@ namespace RPG_Game
             app.UseRouting();
 
             app.UseAuthorization();
+            app.UseSession();
 
             app.UseEndpoints(endpoints =>
             {
